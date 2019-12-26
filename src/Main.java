23d9572d98e1bc183d2032347dc0b295c28c3155
anandanur92 <anandanur92@gmail.com>
2019-12-26 19:56:18 +0700

    Chat Server

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ^_^
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Client form= new Client();
+        form.setVisible(true);
+        Server form1= new Server();
+        form1.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
